4c91de045835008d42e4ce09c56618099ce376df
luv29 <luvkansal29@gmail.com>
2025-01-07 21:00:47 +0530

    login edit and email

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -2,12 +2,16 @@ import { Router } from "express";
 import {
   registerUser,
   loginUser,
+  logoutUser
 } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
+// secured routes
+router.route("/logout").get(verifyJWT, logoutUser)
 
 export default router;
