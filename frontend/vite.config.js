14fe5a870b2d51de1273e64e9938cae0c8b44465
kunj_10 <kunjvgoyal30@gmail.com>
2025-01-07 21:19:57 +0530

    Adding github Oauth

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: "../.env"
+})
+
 
 // https://vite.dev/config/
 export default defineConfig({
