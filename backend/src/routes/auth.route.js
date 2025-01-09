705e5151f70576d960353352c64599b51b9d5e9d
kunj_10 <kunjvgoyal30@gmail.com>
2025-01-10 01:35:24 +0530

    Added Google OAuth2

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,5 +3,6 @@ import { githubCallback } from "../controllers/auth.controller.js";
 
 const router = Router();
 router.route("/github").post(githubCallback);
+router.route("/google").post(googleCallback);
 
 export default router;
