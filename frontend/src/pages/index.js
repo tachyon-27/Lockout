fe345c28c851a08756e5b2d173b4a55955f16f36
luv29 <luvkansal29@gmail.com>
2025-01-12 04:20:54 +0530

    test

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,9 +2,12 @@ import Home from "./HomePage";
 import Login from "./LoginPage";
 import Register from "./RegisterPage";
 import authGithub from "./auth/authGithub";
+import Verify from './auth/VerifyOTP'
+
 export {
     Home,
     Login,
     Register,
     authGithub,
+    Verify
 }
