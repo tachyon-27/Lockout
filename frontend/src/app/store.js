3e3ef66578f18f49411b003df1b54ac771b33282
luv29 <luvkansal29@gmail.com>
2025-01-16 01:35:00 +0530

    bug fixes

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/authSlice'
+import userReducer from '../features/userSlice'
 
 const store = configureStore({
     reducer: {
-        auth: authReducer
+        user: userReducer,
     }
 })
 
