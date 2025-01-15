bbb3e1260dbbbcc99d0704fd431a83cf9e88c255
luv29 <luvkansal29@gmail.com>
2025-01-16 01:33:05 +0530

    Removed redux auth and added cookie auth

--- backend/src/utils/token.js
+++ backend/src/utils/token.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
-export const generateToken = (_id) => {
+export const generateToken = (_id, expiresIn='30d') => {
     return jwt.sign({ _id }, process.env.JWT_SECRET, {
-        expiresIn: '30d',
+        expiresIn,
     })
 }
