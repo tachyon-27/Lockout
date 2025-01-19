392d311f5e7f67be48c583acb733dc69aaf43573
kunj_10 <kunjvgoyal30@gmail.com>
2025-01-19 15:36:07 +0530

    Added View Tournament, TimePicker, TournamentNavbar

--- frontend/src/layout/TournamentLayout.jsx
+++ frontend/src/layout/TournamentLayout.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { cn } from "@/lib/utils";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import TournamentNavBar from "../components/TournamentNavBar";
 
 const TournamentLayout = () => {
   return (
-    <div>TournamentLayout</div>
-  )
-}
+    <div className={cn("grid grid-cols-1 md:grid-cols-[auto_1fr] gap-4 rounded-md p-10 bg-gray-100 dark:bg-black w-full flex-1 mx-auto overflow-hidden h-screen")}>
+      {/* Center the navbar */}
+      <div className="flex justify-center h-full">
+        <TournamentNavBar className="md:w-64 w-full" />
+      </div>
+      <main className="flex-grow mt-4">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 
-export default TournamentLayout
+export default TournamentLayout;
