68c833ce4cdf0ebf9e151aa9f108c6ed5c65460b
luv29 <luvkansal29@gmail.com>
2025-01-20 16:23:29 +0530

    Add tournaments pag

--- frontend/src/components/ui/magic-card.tsx
+++ frontend/src/components/ui/magic-card.tsx
@@ -79,7 +79,7 @@ export function MagicCard({
       className={cn("group relative flex size-full rounded-xl", className)}
     >
       <div className="absolute inset-px z-10 rounded-xl bg-background" />
-      <div className="relative z-30">{children}</div>
+      <div className="w-full relative z-30">{children}</div>
       <motion.div
         className="pointer-events-none absolute inset-px z-10 rounded-xl opacity-0 transition-opacity duration-300 group-hover:opacity-100"
         style={{
