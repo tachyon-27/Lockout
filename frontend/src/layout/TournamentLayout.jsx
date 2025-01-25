bc92e4ce89dbd37d7bf3cc05ca0add42dbb0805d
luv29 <luvkansal29@gmail.com>
2025-01-25 18:34:51 +0530

    part

--- frontend/src/layout/TournamentLayout.jsx
+++ frontend/src/layout/TournamentLayout.jsx
@@ -7,10 +7,10 @@ const TournamentLayout = () => {
   return (
     <div className={cn("grid grid-cols-1 md:grid-cols-[auto_1fr] gap-4 rounded-md p-10 bg-gray-100 dark:bg-black w-full flex-1 mx-auto overflow-hidden h-screen")}>
       {/* Center the navbar */}
-      <div className="flex justify-center h-full">
+      <div className="flex justify-center h-full w-full">
         <TournamentNavBar className="md:w-64 w-full" />
       </div>
-      <main className="flex-grow mt-4">
+      <main className="flex-grow mt-4 w-full">
         <Outlet />
       </main>
     </div>
