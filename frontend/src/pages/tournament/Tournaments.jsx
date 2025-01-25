31c46bb18dd79f031096cc31eaa81d2b4cbd98f5
luv29 <luvkansal29@gmail.com>
2025-01-25 18:32:42 +0530

    Better?

--- frontend/src/pages/tournament/Tournaments.jsx
+++ frontend/src/pages/tournament/Tournaments.jsx
@@ -1,13 +1,15 @@
-import { MagicCard } from "@/components/ui/magic-card";
 import { useNavigate } from "react-router-dom";
 import { useToast } from "@/hooks/use-toast";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { TournamentRegister } from "@/components";
+import { RegistrButton } from "@/components";
 import {
-  Modal,
-  ModalTrigger,
-} from "@/components/ui/animated-modal";
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 
 export default function Tournament({ isAdmin }) {
   if(!isAdmin) isAdmin = false;
@@ -20,10 +22,8 @@ export default function Tournament({ isAdmin }) {
       try {
         const res = await axios.get("/api/tournament/tournaments");
 
-        console.log(res.data.data);
         setTournaments(res.data.data);
       } catch (error) {
-        console.log(error);
         toast({
           title: "Error",
           description: error.response?.data?.message || error.message,
@@ -34,63 +34,46 @@ export default function Tournament({ isAdmin }) {
   }, [toast]);
 
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4">
-  {tournaments.map((tournament, idx) => (
-    <MagicCard
-      key={idx}
-      className="relative h-[50vh] w-full cursor-pointer p-4 bg-black text-white shadow-2xl"
-    >
-      <div className="break-all w-full h-full min-w-full gap-y-3 grid grid-cols-1 grid-rows-6">
-        <div className="text-3xl font-bold">{tournament.title}</div>
+    <Accordion type="single" collapsible className="w-[80vw]">
+      {tournaments.map((tournament, idx) => (
+        <AccordionItem key={idx} value={idx}>
+          <AccordionTrigger> {tournament.title} </AccordionTrigger>
+          <AccordionContent>
+            <div>
+              <p> {tournament.summary} </p>
+         
+              <div className="w-full flex items-center mt-2 gap-2">
+                <button
+                  className="px-4 py-2 rounded-md bg-black text-white flex justify-center border border-solid border-white hover:bg-neutral-900"
+                  onClick={() => navigate(`/tournament/view?id=${tournament._id}`)}
+                >
+                  View
+                </button>
 
-        <div className=" mt-4 text-lg row-span-4">
-          {tournament.summary}
-        </div>
-
-        <div className="w-full flex justify-between items-center">
-          <button
-            className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md"
-            onClick={() => navigate(`/tournament/view?id=${tournament._id}`)}
-            >
-            View
-          </button>
-
-          { isAdmin ? (
-            <div className="flex gap-2">
-            <button
-              onClick={() => navigate(`/admin/dashboard/update-tournament?id=${tournament._id}`)}
-              className="px-4 py-2 bg-green-600 hover:bg-green-700 text-white rounded-md"
-            >
-              Edit
-            </button>
-            <button
-              className="px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-md"
-            >
-              Delete
-            </button>
-          </div>
-          ) : (
-            new Date(tournament.startDate) > new Date() ? 
-            (
-            <Modal>
-              <ModalTrigger className="bg-black dark:bg-white dark:text-black text-white flex justify-center group/modal-btn border border-">
-          <span className="group-hover/modal-btn:translate-x-40 text-center transition duration-500">
-            Book your flight
-          </span>
-          <div className="-translate-x-40 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-white z-20">
-            ✈️
-          </div>
-        </ModalTrigger>
-              <TournamentRegister />
-            </Modal>
-              ) : (<></>)
-          ) }
-        </div>
-      </div>
-    </MagicCard>
-  ))}
-</div>
-
-
+                { isAdmin ? (
+                  <div className="flex gap-2">
+                  <button
+                    onClick={() => navigate(`/admin/dashboard/update-tournament?id=${tournament._id}`)}
+                    className="px-4 py-2 rounded-md bg-black text-white flex justify-center border border-solid border-white hover:bg-neutral-900"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="px-4 py-2 rounded-md bg-black text-white flex justify-center border border-solid border-white hover:bg-neutral-900"
+                  >
+                    Delete
+                  </button>
+                </div>
+                ) : (
+                  new Date(tournament.startDate) > new Date() ? (
+                      <RegistrButton tournamentId={tournament._id}/>
+                    ) : (<></>)
+                ) }
+              </div>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,20 +18,42 @@ export default {
   theme: {
   	extend: {
   		animation: {
-			"caret-blink": "caret-blink 1.25s ease-out infinite",
-  			aurora: 'aurora 60s linear infinite'
+  			'caret-blink': 'caret-blink 1.25s ease-out infinite',
+  			aurora: 'aurora 60s linear infinite',
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		},
   		keyframes: {
-			"caret-blink": {
-			  "0%,70%,100%": { opacity: "1" },
-			  "20%,50%": { opacity: "0" },
-			},
+  			'caret-blink': {
+  				'0%,70%,100%': {
+  					opacity: '1'
+  				},
+  				'20%,50%': {
+  					opacity: '0'
+  				}
+  			},
   			aurora: {
   				from: {
   					backgroundPosition: '50% 50%, 50% 50%'
   				},
   				to: {
   					backgroundPosition: '350% 50%, 350% 50%'
+  				}
+  			},
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
   				}
   			}
   		},
