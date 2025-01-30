9aedbb40304afa8c080e088fa1e9d956ee338c8a
kunj_10 <kunjvgoyal30@gmail.com>
2025-01-31 02:15:11 +0530

    Added the controller for Question status check

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,10 +1,8 @@
-export const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500
-
-    res.status(statusCode)
-
-    res.json({
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500; 
+    res.status(statusCode).json({
         message: err.message,
-    })
-}
-
+        // stack: process.env.NODE_ENV === "development" ? err.stack : null,
+        stack: err.stack ,
+    });
+};
