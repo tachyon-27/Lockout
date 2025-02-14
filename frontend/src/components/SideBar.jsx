c983f0cd7a8873b1ae26adb3f8829be8928d1eea
luv29 <luvkansal29@gmail.com>
2025-02-14 20:52:28 +0530

    Added Admins Page

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -45,7 +45,7 @@ export function SideBar() {
     },
     {
       label: "Admins",
-      href: "#",
+      href: "/admin/dashboard/admins",
       icon: (
         <IconUsersPlus className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
