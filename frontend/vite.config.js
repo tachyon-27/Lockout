73d8de3c840d5c7eab07734719ea1037acb31b28
luv29 <luvkansal29@gmail.com>
2025-02-28 11:55:39 +0530

    deployement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 import path from "path"
 
-// dotenv.config({
-//   path: "../.env"
-// })
+dotenv.config({
+  path: "../.env"
+})
 
 
 // https://vite.dev/config/
